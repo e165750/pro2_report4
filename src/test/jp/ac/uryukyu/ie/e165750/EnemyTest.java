f3b34519af6f8cf60af862e70e762c590d9e8a67
Wakana Iha <e165750@ie.u-ryukyu.ac.jp>
2016-11-22 19:56:10 +0900

    Commit success!

---
+++ src/test/jp/ac/uryukyu/ie/e165750/EnemyTest.java
@@ -0,0 +1,14 @@
+package jp.ac.uryukyu.ie.e165750;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by e165750 on 2016/11/21.
+ */
+public class EnemyTest {
+    @org.junit.Test
+    public void attack() throws Exception {
+
+    }
+
+}
